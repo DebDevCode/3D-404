0d7cf27a876234ade3e7161e63fdf497b0486c31
Luca <luca.debionne@yahoo.fr>
2024-02-26 10:50:15 +0100

    final

--- app/layout.js
+++ app/layout.js
@@ -12,6 +12,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className="overflow-hidden">
       <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale = 1, shrink-to-fit = no"
+        />
         <meta name="theme-color" content="#000000" />
       </head>
       <body className={inter.className}>{children}</body>
