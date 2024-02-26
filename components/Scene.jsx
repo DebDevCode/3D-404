a5ac3a0a3b9afdee826a5bd78eb1cbccc85f5035
Luca <luca.debionne@yahoo.fr>
2024-02-26 03:57:29 +0100

    final

--- components/Scene.jsx
+++ components/Scene.jsx
@@ -2,6 +2,7 @@
 import { Environment } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import Model from "./Model";
+
 export default function Scene() {
   return (
     <Canvas
